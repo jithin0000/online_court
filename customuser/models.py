37db7de23122563baff4713f2088feffd02e187b
jithin0000 <mbjithinbabu@gmail.com>
2020-02-25 13:55:17 +0530

    static and media folders added

--- customuser/models.py
+++ customuser/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """ custom user manager for user """
+    pass
